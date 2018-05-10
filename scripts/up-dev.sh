9487d9ee372a91cc5a4989a57327a9edbed9e5a3
Aaron <gallen83@gmail.com>
2018-05-10 23:27:31 +1000

    docker-compose file change

--- scripts/up-dev.sh
+++ scripts/up-dev.sh
@@ -2,15 +2,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SRC_DIR="$DIR/../src"
 USER=`whoami`
-#check these values
-#echo $DIR
-#echo $SRC_DIR
-#echo $USER
-#cd $DIR/../
-#pwd
 
-
-cd $DIR/../
-sudo chmod -R 777 src/store/app/etc
-sudo chmod -R 777 src/store/media
-docker-compose --file docker-compose.yml --project-name store up -d
+sudo chmod -R 777 $DIR/../src
+docker-compose --file docker-compose.yml up
